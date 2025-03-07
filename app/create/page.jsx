50c0aefc6bacb876b8d087a705abee5776ddbd7c
div-j <divinejohn65@gmail.com>
2025-03-07 16:20:43 +0100

    v1

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -9,6 +9,7 @@ import { useUser } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from 'next/navigation';
+import Header from "../dashboard/_components/Header";
 
 export default function CreatePage() {
   const [step, setStep] = useState(0);
@@ -48,48 +49,51 @@ export default function CreatePage() {
   }
 
   return (
-    <div className="flex flex-col items-center md:px-24 p-5 lg:px-36 mt-20">
-      <div className=' w-full text-white  p-5 flex flex-col items-center gap-6 lg"px-36 mt-10'>
-        <h2 className="font-bold text-4xl text-primary">
-          Start Building Your Personal Study Material
-        </h2>
-        <p className="text-lg text-gray-500 ">
-          Fill add details in order to generate study material for your next
-          project
-        </p>
+  <section>
+    <Header/>  
+      <div className="flex flex-col items-center md:px-24 p-5 lg:px-36 mt-20">
+        <div className=' w-full text-white  p-5 flex flex-col items-center gap-6 lg"px-36 mt-10'>
+          <h2 className="font-bold text-4xl text-primary">
+            Start Building Your Personal Study Material
+          </h2>
+          <p className="text-lg text-gray-500 ">
+            Fill add details in order to generate study material for your next
+            project
+          </p>
+        </div>
+        <section>
+          {step === 0 ? (
+            <SelectOption
+              selectedStudyType={(value) => handleUserInput("studyType", value)}
+            />
+          ) : (
+            <TopicInput
+              setTopic={(value) => handleUserInput("topic", value)}
+              setDifficultyLevel={(value) =>
+                handleUserInput("difficultyLevel", value)
+              }
+            />
+          )}
+        </section>
+        <section className="flex justify-between w-full mt-32">
+          {step != 0 ? (
+            <Button varient="outline" onClick={() => setStep(step - 1)}>
+              Previous
+            </Button>
+          ) : (
+            "-"
+          )}
+          {step == 0 ? (
+            <Button varient="outline" onClick={() => setStep(step + 1)}>
+              Next
+            </Button>
+          ) : (
+            <Button varient="outline" onClick = {generateCourseOutline}>
+              {loading?<Loader/>:"Generate"}
+            </Button>
+          )}
+        </section>
       </div>
-      <section>
-        {step === 0 ? (
-          <SelectOption
-            selectedStudyType={(value) => handleUserInput("studyType", value)}
-          />
-        ) : (
-          <TopicInput
-            setTopic={(value) => handleUserInput("topic", value)}
-            setDifficultyLevel={(value) =>
-              handleUserInput("difficultyLevel", value)
-            }
-          />
-        )}
-      </section>
-      <section className="flex justify-between w-full mt-32">
-        {step != 0 ? (
-          <Button varient="outline" onClick={() => setStep(step - 1)}>
-            Previous
-          </Button>
-        ) : (
-          "-"
-        )}
-        {step == 0 ? (
-          <Button varient="outline" onClick={() => setStep(step + 1)}>
-            Next
-          </Button>
-        ) : (
-          <Button varient="outline" onClick = {generateCourseOutline}>
-            {loading?<Loader/>:"Generate"}
-          </Button>
-        )}
-      </section>
-    </div>
+  </section>
   );
 }
--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <div className='flex justify-between p-5 shadow-md'>
       <div className='flex gap-2 items-center'>
-    <Link href="/dashboard">
         <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+    <Link href="/dashboard">
           <h1 className='text-3xl text-blue-500 font-bold cursor-pointer'>Smart Study</h1>
         </Link>
       </div>
