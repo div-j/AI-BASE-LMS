c10e35d6916e280523a78564b63f419cde93ff33
div-j <divinejohn65@gmail.com>
2025-03-11 11:30:41 +0100

    v3

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,8 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-base-lms" });
+export const inngest = new Inngest({
+     id: "ai-base-lms",
+     eventKey:process.env.INNGEST_EVENT_KEY,
+    
+    });
